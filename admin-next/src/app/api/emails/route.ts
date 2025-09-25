d513e3ce4d1c1f98dc781cc6f4062fd1d73ccf49
CheolheeLee0 <wks0968@gmail.com>
2025-09-26 00:46:04 +0900

    remove: folder

--- admin-next/src/app/api/emails/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { emails } from "@/lib/email-storage";
-
-export async function GET() {
-	return NextResponse.json({
-		count: emails.length,
-		emails: emails.slice(-50),
-	});
-}
